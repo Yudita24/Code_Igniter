1a20487a8603efc1c3e899afc850efc79b21d0c1
Yudita24 <wyudita321@gmail.com>
2020-11-12 12:15:41 +0800

    commit tambah data

--- application/views/mahasiswa_index.php
+++ application/views/mahasiswa_index.php
@@ -1,27 +1,49 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>MAHASISWA</title>
+	<title>Data Mahasiswa</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/bootstrap.min.css') ?>">
 </head>
-<body>
+<body style="background-color: black">
 
-	<h1>MAHASISWA</h1>
-	<h3>Selamat datang, <?php echo $nama_lengkap ?></h3>
-	<h3>Jenis Kelamin, <?php echo $jenis_kelamin ?></h3>
-	<h3>Nim, <?php echo $nim ?></h3>
-	<hr>
-	<a href="<?php echo site_url('mahasiswa/tambah') ?>">Tambah Data</a>
-	<table border="1">
-		<tr>
-			<th>Nim</th>
-			<th>Nama</th>
-		</tr>
-		<?php foreach ($mahasiswa as $key): ?>
-			<tr>
-				<td><?php echo $key['nim'] ?></td>
-				<td><?php echo $key['nama'] ?></td>
+	<nav class="navbar navbar-expand-lg navbar-dark shadow" style="background-color: purple">
+		<a href="" class="navbar-brand">
+			Tugas
+		</a>
+		<div class="navbar-nav">
+			<a href="<?php echo site_url('mahasiswa') ?>" class="nav-link active">Home</a>
+			<a href="<?php echo site_url('mahasiswa/tambah') ?>" class="nav-link">Tambah Data</a>
+		</div>
+		<div class="navbar-nav ml-auto">
+			<a href="" class="nav-link">I Wayan Yudita Pratama</a>
+		</div>
+	</nav>
+
+	<!-- mt-0 adalah singkatan dari margin top ukuran 3 -->
+	<div class="alert alert-warning text-center mt-0">
+		Data Mahasiswa
+	</div>
+	<div class="container">
+		<table class="table table-bordered table-sm">
+			<tr class="text-center">
+				<th width="1%">NO</th>
+				<th>NIM</th>
+				<th>NAMA</th>
+				<th>ALAMAT</th>
+				<th width="12%">AKSI</th>
 			</tr>
-		<?php endforeach ?>
-	</table>
+			<tr class="text-center">
+				<td>1</td>
+				<td>1810330021</td>
+				<td>I Wayan Yudita Pratama</td>
+				<td>Seraya Pagesangan</td>
+				<td>
+					<a href="" class="btn btn-info btn-sm">Edit</a>
+					<a href="" class="btn btn-danger btn-sm">Hapus</a>
+				</td>
+			</tr>
+		</table>
+	</div>
+	
 </body>
 </html>
--- application/views/mahasiswa_tambah.php
+++ application/views/mahasiswa_tambah.php
@@ -1,22 +1,58 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Mahasiswa</title>
+	<title>Tambah Mahasiswa</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/bootstrap.min.css') ?>">
 </head>
 <body>
 
-	<h1>Tambah Mahasiswa</h1>
-	<hr>
-	<a href="<?php echo site_url('mahasiswa') ?>">kembali</a>
-	<form>
-		<h6>Nim</h6>
-		<input type="number" name="nim"><br>
+	<!-- NAVBAR -->
+	<nav class="navbar navbar-expand-lg navbar-dark shadow" style="background-color: purple">
+		<a href="" class="navbar-brand">TUGAS</a>
+		<div class="navbar-nav">
+			<a href="<?php echo site_url('mahasiswa') ?>" class="nav-link">Home</a>
+			<a href="<?php echo site_url('mahasiswa/tambah') ?>" class="nav-link active">Tambah Data</a>
+		</div>
+		<div class="navbar-nav ml-auto">
+			<a href="" class="nav-link">I Wayan Yudita Pratama</a>
+		</div>
+	</nav>
+	<!-- NAVBAR END -->
+	
+	<!-- Alert -->
+	<div class="alert alert-warning text-center mt-0">
+		Tambah Data Mahasiswa
+	</div>
+	<!-- Alert END -->
 
-		<h6>Nama</h6>
-		<input type="text" name="nama_mahasiswa"> <br>
 
-		<input type="submit" name="submit" value="simpan">
-	</form>
+	<!-- FORM TAMBAH DATA -->
+	<div class="container">
+		<div class="card">
+			<div class="card-body">
+				<form>
+					<div class="form-group">
+						<label for="nim">NIM Mahasiswa</label>
+						<input type="number" class="form-control" id="nim">
+					</div>
+					<div class="form-group">
+						<label for="nama">Nama Mahasiswa</label>
+						<input type="text" class="form-control" id="nama">
+					</div>
+					<div class="form-group">
+						<label for="alamat">Alamat Mahasiswa</label>
+						<textarea class="form-control" id="alamat"></textarea>
+					</div>
+					<button type="submit" class="btn btn-success"><svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-plus" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+						<path fill-rule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+					</svg> Input Data Mahasiswa</button>
+				</form>
+			</div>
+			
+		</div>
+		
+	</div>
+	<!-- FORM TAMBAH DATA END -->
 
 </body>
 </html>
